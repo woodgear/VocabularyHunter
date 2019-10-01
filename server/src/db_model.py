79945980a6f500937656827af557cc474893e089
wucong <1875486458@qq.com>
2019-10-01 13:53:32 +0800

    fix for test

--- server/src/db_model.py
+++ server/src/db_model.py
@@ -3,6 +3,7 @@ import time as pytime
 import os
 DB_VERSION = 1
 
+
 class DbModel:
     """the abstract layer of vh.
 
@@ -10,48 +11,49 @@ class DbModel:
 
     Attributes:
         db: string the data base name,just to idenity.
-    
+
     """
 
-    def __init__(self,db_name="vh",folder_path="db_storage"):
-        self.db_name=db_name
-        self.folder_path=folder_path
+    def __init__(self, db_name="vh", folder_path="db_storage"):
+        self.db_name = db_name
+        self.folder_path = folder_path
         self._init_db()
         pass
+
     def __del__(self):
-        self.db.close()
-        pass  
+        if hasattr(self, "db"):
+            self.db.close()
+        pass
 
     def _init_db(self):
-        try:
-            sqlite_uri = f'sqlite://{self.db_name}.sqlite'
-            self.db = DAL(sqlite_uri, folder=self.folder_path)
-            self.db.define_table('word', Field('user_id'),Field('word'),Field('word_type'),Field('time'))
-            self.db.define_table('meta', Field('key'),Field('value'))
-            self.db.meta.insert(key='db_version',value=DB_VERSION)
-            self.db.commit()
-        except Exception as Err:
-            print("err",Err)
+        sqlite_uri = f'sqlite://{self.db_name}.sqlite'
+        self.db = DAL(sqlite_uri, folder=self.folder_path)
+        self.db.define_table('word', Field('user_id'), Field(
+            'word'), Field('word_type'), Field('time'))
+        self.db.define_table('meta', Field('key'), Field('value'))
+        self.db.meta.insert(key='db_version', value=DB_VERSION)
+        self.db.commit()
 
-        pass
-    def get_all_unknow_word_by_id(self,id):
+    def get_all_unknow_word_by_id(self, id):
         """
         Args:
             id: string user id
         Returns:
             [string]
         """
-        return [item.word for item in self.db((self.db.word.user_id==id) & (self.db.word.word_type=="unknow")).select(self.db.word.ALL)]
-    def get_all_know_word_by_id(self,id):
+        return [item.word for item in self.db((self.db.word.user_id == id) & (self.db.word.word_type == "unknow")).select(self.db.word.ALL)]
+
+    def get_all_know_word_by_id(self, id):
         """
         Args:
             id: string user id
         Returns:
             [string]
         """
-        return [item.word for item in self.db((self.db.word.user_id==id) & (self.db.word.word_type=="know")).select(self.db.word.ALL)]
+        return [item.word for item in self.db((self.db.word.user_id == id) & (self.db.word.word_type == "know")).select(self.db.word.ALL)]
         pass
-    def mark_know_word_by_id(self,id,words):
+
+    def mark_know_word_by_id(self, id, words):
         """
         Args:
             id: string user id
@@ -59,15 +61,15 @@ class DbModel:
         """
         for word in words:
             self.db.word.update_or_insert(
-            (self.db.word.word == word) & (self.db.word.user_id == id) ,
-            word=word,
-            user_id=id,
-            word_type="know",
-            time=pytime.asctime(pytime.gmtime()))        
+                (self.db.word.word == word) & (self.db.word.user_id == id),
+                word=word,
+                user_id=id,
+                word_type="know",
+                time=pytime.asctime(pytime.gmtime()))
         self.db.commit()
         pass
 
-    def mark_unknow_word_by_id(self,id,words):
+    def mark_unknow_word_by_id(self, id, words):
         """
         Args:
             id: string user id
@@ -75,10 +77,10 @@ class DbModel:
         """
         for word in words:
             self.db.word.update_or_insert(
-            (self.db.word.word == word) & (self.db.word.user_id == id) ,
-            word=word,
-            user_id=id,
-            word_type="unknow",
-            time=pytime.asctime(pytime.gmtime()))
+                (self.db.word.word == word) & (self.db.word.user_id == id),
+                word=word,
+                user_id=id,
+                word_type="unknow",
+                time=pytime.asctime(pytime.gmtime()))
         self.db.commit()
-        pass        
+        pass
