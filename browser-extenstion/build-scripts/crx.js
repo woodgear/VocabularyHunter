6ed29a4ed4493d3d98088b6c0871e4fd38b18c47
wucong <1875486458@qq.com>
2020-01-01 15:10:28 +0800

    try support to github actions

--- browser-extenstion/build-scripts/crx.js
+++ browser-extenstion/build-scripts/crx.js
@@ -2,12 +2,21 @@ const fs = require('fs')
 
 const ChromeExtension = require('crx')
 
+function getPem() {
+  const pemFile = './secret/vh-chrome-extension.pem'
+  if (fs.existsSync(pemFile)) {
+    return fs.readFileSync(pemFile)
+  }
+  return process.env["CHROME_PEM"]
+}
+
 async function build () {
   const crx = new ChromeExtension({
-    privateKey: fs.readFileSync('./secret/vh-chrome-extension.pem')
+    privateKey: getPem()
   })
   const crxBuffer = await crx.load('./dist').then((crx) => crx.pack())
   fs.writeFileSync('./vh.crx', crxBuffer)
 }
 
 build()
+console.log("build over");
