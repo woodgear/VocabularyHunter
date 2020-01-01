63d66e273e657b97df3e964c29aacb60a88395f4
wucong <1875486458@qq.com>
2020-01-01 14:50:27 +0800

    fix build crx

--- browser-extenstion/build-scripts/crx.js
+++ browser-extenstion/build-scripts/crx.js
@@ -4,7 +4,7 @@ const ChromeExtension = require('crx')
 
 async function build () {
   const crx = new ChromeExtension({
-    privateKey: fs.readFileSync('./vh-chrome-extension.pem')
+    privateKey: fs.readFileSync('./secret/vh-chrome-extension.pem')
   })
   const crxBuffer = await crx.load('./dist').then((crx) => crx.pack())
   fs.writeFileSync('./vh.crx', crxBuffer)
