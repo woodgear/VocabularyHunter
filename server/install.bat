88c5846505d7feb632fc9329eec135e157399c13
wucong <1875486458@qq.com>
2020-01-01 17:21:25 +0800

    ad vh-server ci

--- server/install.bat
+++
@@ -1,7 +0,0 @@
-pip install pyfunctional
-pip install nltk
-pip install orderedset
-pip install pyDAL
-pip install flask
-pip install flask-cors
-python src/setup.py
--- server/install.sh
+++ server/install.sh
@@ -1,9 +1,1 @@
-pip3 install pyfunctional --user
-pip3 install nltk --user
-pip3 install orderedset --user
-pip3 install pyDAL --user
-pip3 install flask --user
-pip3 install flask-cors --user
-pip3 install  waitress-serve --user
-pip install Flask-gzip --user
-
+pip3 install -r ./requirements.txt
